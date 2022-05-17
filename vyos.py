5f6108a6182e7c3ab9a8f569025d0dbcfba7961d
unknown <paulsherwood.basongit@gmail.com>
2022-05-17 11:41:12 +0800

    Adding lines for vyos

--- vyos.py
+++ vyos.py
@@ -7,6 +7,20 @@ from netmiko import *
 import ping
 import json
 
+vyos1= {
+    "device_type": "vyos",
+    "host": "192.168.1.15",
+    "username": "vyos",
+    "password": "$75C.woodie"
+}
+
+vyos2= {
+    "device_type": "vyos",
+    "host": "192.168.1.16",
+    "username": "vyos",
+    "password": "$75C.woodie"
+}
+
 with open("vyos_details.json", "r") as login_details:
     login= json.load(login_details)
 
@@ -16,10 +30,19 @@ class Network:
     
     def mac_addr(self):
         '''Output mac address from the vyos router'''
-        main_conn= ConnectHandler(**login)
-
+        
 #Check output using ping.py module.
 for ip_addr in range(15, 17):
     octet= "192.168.1."
     ip= str(ip_addr)
     ping.ping_conn(f'{octet}{ip}')
+
+
+dev_list= [vyos1, vyos2]
+for device in dev_list:
+    try:
+        main_conn= ConnectHandler(**device)
+        print(f"{octet}{ip} ---> Connection Established")
+
+    except(NetMikoAuthenticationException, NetMikoTimeoutException) as net_err:
+        print(net_err)
