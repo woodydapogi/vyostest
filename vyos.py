3c689f0f976e8962176c10ea730139730917f7ce
unknown <paulsherwood.basongit@gmail.com>
2022-05-17 11:17:35 +0800

    Created initial vyos config

---
+++ vyos.py
@@ -0,0 +1,20 @@
+from netmiko import *
+import ping
+import json
+
+with open("vyos_details.json", "r") as login_details:
+    login= json.load(login_details)
+
+class Network:
+    def __init__(self, mac_id):
+        self.mac_id = mac_id
+    
+    def mac_addr(self):
+        '''Output mac address from the vyos router'''
+        main_conn= ConnectHandler(**login)
+
+
+for ip_addr in range(15, 17):
+    octet= "192.168.1."
+    ip= str(ip_addr)
+    ping.ping_conn(f'{octet}{ip}')
